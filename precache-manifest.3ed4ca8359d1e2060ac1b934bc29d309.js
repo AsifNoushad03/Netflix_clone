852d9f6c2c6599cbc4df67387c8e250f091519dd
unknown <asifnoushad61@gmail.com>
2022-08-27 16:24:39 +0530

    Updates

---
+++ precache-manifest.3ed4ca8359d1e2060ac1b934bc29d309.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4b7f4d8c8517730f7e9",
+    "url": "/netlix-Clone/static/css/main.40f54078.chunk.css"
+  },
+  {
+    "revision": "d4b7f4d8c8517730f7e9",
+    "url": "/netlix-Clone/static/js/main.d4b7f4d8.chunk.js"
+  },
+  {
+    "revision": "9afad71ae023148390d5",
+    "url": "/netlix-Clone/static/js/1.9afad71a.chunk.js"
+  },
+  {
+    "revision": "0527652e91b44b896336",
+    "url": "/netlix-Clone/static/js/runtime~main.0527652e.js"
+  },
+  {
+    "revision": "1ef88eea854077bbd69897697a06d057",
+    "url": "/netlix-Clone/index.html"
+  }
+];
